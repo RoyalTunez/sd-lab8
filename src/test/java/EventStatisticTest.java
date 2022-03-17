b5951facc2ffda73c4f6a89e5ff77de0e7f4b94c
tunez <myironmistake@gmail.com>
2022-03-17 17:26:24 +0300

    add new random test for eventStatistic

--- src/test/java/EventStatisticTest.java
+++ src/test/java/EventStatisticTest.java
@@ -7,8 +7,7 @@ import statistic.RpmStatistic;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class EventStatisticTest {
     private Instant now;
@@ -16,7 +15,6 @@ public class EventStatisticTest {
     private EventStatistic eventsStatistic;
 
     private final static int HOUR_TO_MINUTES = 60;
-    private final static int HOUR_TO_SECONDS = 3600;
     private final static double MAX_ERROR = 1e-9;
 
     @BeforeEach
@@ -71,5 +69,56 @@ public class EventStatisticTest {
         clock.setNow(now.plus(61, ChronoUnit.MINUTES));
         Assertions.assertEquals(1.0 / 60.0, eventsStatistic.getEventStatisticByName("firstEvent"), MAX_ERROR);
         Assertions.assertEquals(0.0, eventsStatistic.getEventStatisticByName("secondEvent"), MAX_ERROR);
+    }
+
+    @Test
+    public void testRandomStatistic() {
+        final int TEST_SIZE = 10000;
+        final int EVENTS_SIZE = 200;
+
+        Random random = new Random();
+
+        Map<String, Integer> eventsCounter = new HashMap<>();
+
+        for (int eventId = 0; eventId <= EVENTS_SIZE; eventId++) {
+            eventsCounter.put(String.valueOf(eventId), 0);
+        }
+
+        Deque<String> eventList = new ArrayDeque<>();
+
+        for (int it = 0; it < TEST_SIZE; it++) {
+            clock.setNow(now.plus(it, ChronoUnit.MINUTES));
+
+            String eventName = String.valueOf(random.nextInt(EVENTS_SIZE));
+
+            eventList.addLast(eventName);
+
+            int oldNewEventCounter = 0;
+
+            try {
+                oldNewEventCounter = eventsCounter.get(eventName);
+            } catch (Exception e) {
+                System.out.println(eventName);
+            }
+
+            eventsCounter.replace(eventName, oldNewEventCounter + 1);
+
+            if (eventList.size() > HOUR_TO_MINUTES) {
+                String expiredEventName = eventList.pollFirst();
+
+                int oldExpiredEventCounter = eventsCounter.get(expiredEventName);
+
+                eventsCounter.replace(expiredEventName, oldExpiredEventCounter - 1);
+            }
+
+            eventsStatistic.incEvent(eventName);
+
+            for (var eventCounter : eventsCounter.entrySet()) {
+                String checkEventName = eventCounter.getKey();
+                double expectedEventRpm = (double)eventCounter.getValue() / HOUR_TO_MINUTES;
+
+                Assertions.assertEquals(expectedEventRpm, eventsStatistic.getEventStatisticByName(checkEventName), MAX_ERROR * (it + 1));
+            }
+        }
     }
 }
